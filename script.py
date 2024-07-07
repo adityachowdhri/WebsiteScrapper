89cbc4a0de36a3a2ee41d340c04920c130aeb5b8
adityachowdhri <59203555+adityachowdhri@users.noreply.github.com>
2024-07-07 12:13:40 +0530

    Initial commit

--- script.py
+++ script.py
@@ -13,13 +13,16 @@ from email.mime.multipart import MIMEMultipart
 import time 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
+from helper import *
 
-
-uri = "mongodb+srv://adityachowdhri123:tJbHc2PmElzsvLHN@webscraping.9rrm6xc.mongodb.net/?appName=WebScraping"
+uri = os.getenv("DATABASE_URL")
+emailId = os.getenv("EMAIL_ID")
+passkey = os.getenv("EMAIL_PASSKEY")
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
-
 
 
 
@@ -42,53 +45,11 @@ for link in documentLinks.find():
             old_text = doc["content"]
             question = f"""What is the difference in words between these texts: Text 1: {cleaned_text} Text 2: {old_text}"""
             response = ollama.generate(model='llama3', prompt=question)
-            sendEmail(link["_id"] , "kavitastarot@gmail.com", "hdkr mdjl bttz jcml", ["adityachowdhri123@gmail.com"], response)
+            sendEmail(link["_id"] , emailId, passkey, ["adityachowdhri123@gmail.com"], response)
             
     else:
         print(createNewRecord(documentInformation, link["_id"]))
         
         
-def sendEmail(url, senderAddress, passkey, recievers, message):
-    smtp = smtplib.SMTP('smtp.gmail.com', 587) 
-    smtp.ehlo() 
-    smtp.starttls() 
-    smtp.login(senderAddress, passkey)
-    msg = MIMEMultipart() 
-    msg['Subject'] = "Change to " + url  
-    msg.attach(MIMEText(message))
-    smtp.sendmail(from_addr=senderAddress, to_addrs=recievers, msg=msg.as_string()) 
-    smtp.quit()
-
-
-def createNewRecord(document, link):
-
-    content = requests.get(link).text
-    soup = BeautifulSoup(content, 'html.parser')
-    text = soup.body.get_text()
-    cleaned_text = re.sub(r'\s+', ' ', text)
-    cleaned_text_bytes = cleaned_text.encode('utf-8')
-    hash_object = hashlib.sha256(cleaned_text_bytes)
-    hashed_text = hash_object.hexdigest()
-    info = {"_id": link, "content": cleaned_text, "hash": hashed_text}
-    document.insert_one(info)
-    return info
-
-def connectOutput():
-    db_name = "WebScrappingInformation"
-    db = client[db_name]
-
-# Specify the collection name
-    collection_name = "information"
-    collection = db[collection_name]
-    return collection
-
-def connectInput():
-    db_name = "WebScrappingInformation"
-    db = client[db_name]
-
-# Specify the collection name
-    collection_name = "websites"
-    collection = db[collection_name]
-    return collection
 
 
